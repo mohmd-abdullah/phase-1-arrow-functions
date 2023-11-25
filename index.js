19b944494c9135c15ced243a973a3287b992720e
Mohammad <mo7amed.2197@gmail.com>
2023-11-25 09:53:27 +0300

    Done

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function expression called divide
+const divide = function(a, b) {
+    return a / b;
+  }
+  
+  // Arrow function called square
+  const square = x => x * x;
+  
+  // Arrow function called add
+  const add = (a, b) => a + b;
